4bc605c292f1d1bb2c64336e1d9d2ade64975a08
Mario Ramalho <mario@ramalho.ch>
2024-10-20 21:58:14 +0200

    Initial commit

---
+++ src/e3_226b/Application.java
@@ -0,0 +1,47 @@
+package e3_226b;
+
+//
+// ######################################################################
+// #                        _       _        ____  ____   __            #
+// #        /\/\   ___   __| |_   _| | ___  |___ \|___ \ / /_           #
+// #       /    \ / _ \ / _` | | | | |/ _ \   __) | __) | '_ \          #
+// #      / /\/\ \ (_) | (_| | |_| | |  __/  / __/ / __/| (_) |         #
+// #      \/    \/\___/ \__,_|\__,_|_|\___| |_____|_____|\___/  RAM     #
+// #                        __                                          #
+// #                       |_   |    /  \  \_/                          #
+// #                       |    |__  \__/  / \                          #
+// #                                                                    #
+// ######################################################################
+// # Projet de base pour l'évaluation E3 pour le module 226b.           #
+// # Application console à utiliser pour l'E3                           #
+// ######################################################################
+// # Ecrit par # Mario Ramalho      # VERSION # 1.0 # DATE # 14.06.2024 #
+// ######################################################################
+//
+/**
+ * Cette classe sera utilisée pour exéctuer le projet console.
+ *
+ * @author <a href="mailto:mario.ramalho@edufr.ch">Mario Ramalho</a>
+ * @version 1.0
+ * @since 14.06.2024
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // MERCI DE DEPOSER VOS FICHERS DANS LE REPERTOIRE "files"
+        String fichier_nouveau = "./src/e3_226b/files/nouveau.txt";
+        
+        // Affichage des resultats 
+        System.out.println("----------------------------------");
+        System.out.println("Demarrage de l'application");
+        System.out.println("----------------------------------");
+        
+        // Votre code ici ....
+        
+    }
+    
+}
